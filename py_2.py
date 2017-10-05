3cc3d78bdc77647e3bf187a66af547aa1127e798
Adam Albright <aalbright425@gmail.com>
2017-10-05 02:49:53 -0400

    Started messing with lists

---
+++ py_2.py
@@ -0,0 +1,21 @@
+
+# Lets play with some Lists.
+# TODO: go through https://docs.python.org/3.6/library/stdtypes.html#sequence-types-list-tuple-range.
+# TODO: Then go through https://docs.python.org/3.6/faq/programming.html#faq-multidimensional-list
+# Tired now...
+
+# Print something with a newline character appended.
+def println(thing):
+    print(thing.__str__() + '\n')
+
+
+def main():
+    # Here we are asking if the substring "gg" is contained within "eggs"
+    println("gg" in "eggs")
+
+    # no regex for you
+    println("*g" in "eggs")
+
+# Main program entry point.
+main()
+
