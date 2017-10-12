53f2501e86e96694f2455a947e504e26dbea696a
Adam Albright <aalbright425@gmail.com>
2017-10-12 15:52:24 -0400

    Not using the xml data anymore

--- Src/bz2_storage_testing.py
+++ Src/bz2_storage_testing.py
@@ -9,7 +9,6 @@ import argparse
 import os
 
 
-
 # The user will specify the directory containing the image data and associated metadata.
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_dir", required=True, help="Path to folder containing image and ground_truths directories.")
@@ -35,9 +34,60 @@ def filter_by_contents(seq, value = []):
             if v in el:
                 yield el
 
+def get_data_from_text(path):
+    my_data = []
+    with open(path) as f:
+        temp_data = f.readlines()
+    temp_data = [x.strip() for x in temp_data]
+    for text_data in temp_data:
+        text=text_data.split("=")
+        my_data.append((text[0], text[1]))
+    return my_data
+
+# Get the Image paths, then get the data once we know what paths were are accessing.
+# Got some of the file processing info from:
+# https://stackoverflow.com/questions/3277503/how-do-i-read-a-file-line-by-line-into-a-list
+def get_image_paths():
+    path_list = []
+    data_path = os.path.join(a.input_dir, dir_list(a.input_dir)[0])
+    # The path to all the data
+    data_path = os.path.join(data_path, os.listdir(data_path)[0])
+    # The path to all the images
+    images_path = os.path.join(a.input_dir, dir_list(a.input_dir)[1])
+
+    print(os.listdir(images_path))
+    for item in os.listdir(images_path):
+        my_images_path = os.path.join(images_path, item)
+        my_data_path = os.path.join(data_path, item)
+        myImages_list = os.listdir(my_images_path)
+        myData_list = os.listdir(my_data_path)
+        print(my_data_path)
+        image_data_pairs = []
+        temp_image_list = []
+        temp_data_list = []
+        # Get the Front Images
+        for i in filter_by_contents(myImages_list, ["_fa", "_fb"]):
+            temp_image_list.append(i)
+        # Get the data for the front images
+        for i in filter_by_contents(myData_list, ["_fa", "_fb"]):
+            temp_data_list.append(i)
+        # if we have the same amount  of data entries as we have for images, we are good to go.
+        if len(temp_data_list) == len(temp_image_list):
+            count = 0
+            for img_p in temp_image_list:
+                parsed_data = get_data_from_text(os.path.join(my_data_path, temp_data_list[count]))
+                # print(parsed_data)
+                path_list.append((img_p, parsed_data))
+                count += 1
+    return path_list
+
+def decompress_image(img_path, dest_path):
+    print("Hallo")
+
 
 def main():
     print(dir_list(a.input_dir))
+    print(get_image_paths())
     image_dir = "C:/Workspace/colorferet/colorferet/dvd1/data/images/00001"
     images = os.listdir(image_dir)
     print(images)
@@ -45,18 +95,21 @@ def main():
     img = []
 
     # generate a list of images in a directory.
-    for i in filter_by_contents(images, ["_fa_", "_fb_"]):
+    for i in filter_by_contents(images, ["_fa", "_fb"]):
         img.append(i)
 
     # From https://stackoverflow.com/questions/16963352/decompress-bz2-files
     # make sure that there are images in the list.
     assert (len(img) > 0), "No Image in folder..."
     for im in img:
+
         # Set up input image and stuff
         bz_image_path = os.path.join(image_dir, im)
+
         # Setup output image format and stuff
         ppm_image_path = a.out_dir + im[:-4]
         print(bz_image_path)
+
         with open(ppm_image_path, 'wb') as new_file, bz2.BZ2File(bz_image_path, 'rb') as file:
             # What does the 100*1024 do?
             for data in iter(lambda: file.read(100 * 1024), b''):
