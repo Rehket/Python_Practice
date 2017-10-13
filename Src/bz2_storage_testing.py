c052b49b97def6ba2d6c23994052dc7df560a9d9
Adam Albright <aalbright425@gmail.com>
2017-10-12 20:44:14 -0400

    Works. CD 1 has been decomressed.

--- Src/bz2_storage_testing.py
+++ Src/bz2_storage_testing.py
@@ -2,6 +2,10 @@
 # which contain the image metadata. The directories containing the images and the metadata are adjacent to one another.
 # Each directory containing images has information about the images encoded into the image name.
 
+# Extracts All the front facing images of the people.
+
+
+
 # TODO: Set up xml parser and cv2image tools
 
 import bz2
@@ -77,7 +81,7 @@ def get_image_paths():
             for img_p in temp_image_list:
                 parsed_data = get_data_from_text(os.path.join(my_data_path, temp_data_list[count]))
                 # print(parsed_data)
-                path_list.append((img_p, parsed_data))
+                path_list.append((os.path.join(my_images_path, img_p), parsed_data[8], parsed_data[13], parsed_data[14], parsed_data[15]))
                 count += 1
     return path_list
 
@@ -87,33 +91,31 @@ def decompress_image(img_path, dest_path):
 
 def main():
     print(dir_list(a.input_dir))
-    print(get_image_paths())
+    images_data_paths = get_image_paths()
     image_dir = "C:/Workspace/colorferet/colorferet/dvd1/data/images/00001"
     images = os.listdir(image_dir)
     print(images)
 
-    img = []
-
-    # generate a list of images in a directory.
-    for i in filter_by_contents(images, ["_fa", "_fb"]):
-        img.append(i)
-
     # From https://stackoverflow.com/questions/16963352/decompress-bz2-files
     # make sure that there are images in the list.
-    assert (len(img) > 0), "No Image in folder..."
-    for im in img:
-
+    assert (len(images_data_paths) > 0), "No Image in folder..."
+    counter = 1
+    for im in images_data_paths:
+        counter += 1
         # Set up input image and stuff
-        bz_image_path = os.path.join(image_dir, im)
+        bz_image_path = im[0]
 
         # Setup output image format and stuff
-        ppm_image_path = a.out_dir + im[:-4]
-        print(bz_image_path)
+        ppm_image_path = os.path.join(a.out_dir, str(counter) +".ppm")
+        print(bz_image_path + " to " + ppm_image_path)
+
+
 
         with open(ppm_image_path, 'wb') as new_file, bz2.BZ2File(bz_image_path, 'rb') as file:
             # What does the 100*1024 do?
             for data in iter(lambda: file.read(100 * 1024), b''):
                 new_file.write(data)
+
 
 
 # Main Entry point
